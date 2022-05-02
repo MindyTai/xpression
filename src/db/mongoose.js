1de6a722e4b6929362b5f1063f31974d715a03d0
mindy_tai <mindy_tai@trendmicro.com>
2022-05-02 18:02:37 +0800

    feat: enable mongodb service with docker-compose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`);  
+        await mongoose.connect(`mongodb://mongo:27017/task-manager-api`);  
         console.log('Connected to database')
     } catch (error) {
         console.log('Fail to connect database')
